142ce2b3c77decc50ab2e403df7a0b2bcf7e0bb2
Fiona Qu <76263832+fiona-qu@users.noreply.github.com>
2024-08-19 15:21:10 -0400

    Add updated run.sh for RNA_FM fitness baseline

---
+++ fitness/baseline_scoring_scripts/RNA_FM/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export model_location="path/to/model/location/RNA-FM/fm/"
+export reference_sheet="path/to/reference_sheet.csv"
+export dms_data_dir="path/to/dms/data/dir"
+export output_scores_dir="path/to/output/scores/dir"
+
+
+python compute_fitness.py  \
+    --model_location "$model_location" \
+    --reference_sequences "$reference_sheet" \
+    --dms_directory "$dms_data_dir" \
+    --output_directory "$output_scores_dir" 
