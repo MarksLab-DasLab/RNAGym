17ded017928232e0a7184df75c3ba16b7ee9b290
Fiona Qu <76263832+fiona-qu@users.noreply.github.com>
2024-08-19 15:31:44 -0400

    Update run.sh with more detail on model_location

--- fitness/baseline_scoring_scripts/RNA_FM/run.sh
+++ fitness/baseline_scoring_scripts/RNA_FM/run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+# the directory containing RNA-FM model
+# downloaded from https://github.com/ml4bio/RNA-FM 
 export model_location="path/to/model/location/RNA-FM/fm/"
+
 export reference_sheet="path/to/reference_sheet.csv"
 export dms_data_dir="path/to/dms/data/dir"
 export output_scores_dir="path/to/output/scores/dir"
-
 
 python compute_fitness.py  \
     --model_location "$model_location" \
