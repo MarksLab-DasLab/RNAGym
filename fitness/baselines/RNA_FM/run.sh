5be54d055b6e42f7994ca0c8f9556adc98a80ae2
Fiona Qu <76263832+fiona-qu@users.noreply.github.com>
2025-04-10 18:08:17 -0400

    updated run files for RNA-FM
    extra error handling and options for wt-marginals and masked-marginals scoring

--- fitness/baselines/RNA_FM/run.sh
+++ fitness/baselines/RNA_FM/run.sh
@@ -8,8 +8,10 @@ export reference_sheet="path/to/reference_sheet.csv"
 export dms_data_dir="path/to/dms/data/dir"
 export output_scores_dir="path/to/output/scores/dir"
 
+
 python compute_fitness.py  \
-    --model_location "$model_location" \
-    --reference_sequences "$reference_sheet" \
-    --dms_directory "$dms_data_dir" \
-    --output_directory "$output_scores_dir" 
+--model_location "$model_location" \
+--reference_sequences "$reference_sheet" \
+--dms_directory "$dms_data_dir" \
+--output_directory "$output_scores_dir"\
+--scoring-strategy "masked-marginals" 
