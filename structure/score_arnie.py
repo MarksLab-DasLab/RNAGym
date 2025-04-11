c7b62b3480c3745b493fb00605f21458e0ebaa92
alenkran <choe.christian@gmail.com>
2025-04-11 16:36:49 -0700

    Updated structure pipeline for arnie
    Now compatible with the new train/test data format.

--- structure/score_arnie.py
+++ structure/score_arnie.py
@@ -3,9 +3,30 @@ import os
 from typing import List, Dict
 import pandas as pd
 from tqdm import tqdm
+import numpy as np
 from arnie.bpps import bpps
 from utils import (load_data, unpaired_probabilities, process_predictions, 
-                   compute_performance_metrics, save_predictions, save_performance_metrics)
+                   compute_performance_metrics, save_predictions, save_performance_metrics,
+                   array_to_string, string_to_array)
+
+def organize_data(df: pd.DataFrame) -> pd.DataFrame:
+    # Organize the DMS and 2A3 data
+    df_DMS = df[df["modifier"] == "DMS"]
+    df_2A3 = df[df["modifier"] == "2A3"]
+
+    for df_mod in [df_DMS, df_2A3]:
+        # Remove duplicate sequences within each df. Select the duplicate with the highest signal-to-noise ratio (SNR)
+        df_mod = df_mod.sort_values(['sequence', 'SNR'], ascending=[False, False])
+        df_mod = df_mod.drop_duplicates(subset=['sequence'], keep="first")
+    df = pd.merge(df_DMS, df_2A3, on='sequence', how='outer', suffixes=('_DMS', '_2A3'))
+    df = df[["seqID_DMS", "seqID_2A3", "sequence", "reactivity_DMS", "reactivity_2A3"]]
+    return df
+
+def predict_structures_arnie(seq: str, model_type: str) -> np.ndarray:
+    # clean sequence
+    seq = seq.replace("T", "U") # Convert to RNA if there's any T's by accident
+    pred = bpps(seq, package=model_type)
+    return unpaired_probabilities(pred)
 
 def setup_argparse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="RNA Structure Prediction using Arnie")
@@ -16,40 +37,34 @@ def setup_argparse() -> argparse.Namespace:
     parser.add_argument("--performance_file", type=str, default="performance_SS_pred.csv", help="Name of performance summary file")
     return parser.parse_args()
 
-def predict_structures_arnie(seq: str, model_type: str) -> np.ndarray:
-    pred = bpps(seq, package=model_type)
-    return unpaired_probabilities(pred)
 
 def main(args: argparse.Namespace):
     # Setup paths
     test_data_path = os.path.join(args.data_folder, 'test_data', args.test_data_name)
     output_path = os.path.join(args.output_folder, f'predictions_{args.model_type}.csv')
-    
+    os.makedirs(args.output_folder, exist_ok=True)
+
     # Load data
-    test_data = load_data(test_data_path)
-    
+    df = load_data(test_data_path)
+
+    # Organize and clean up such that each row is a unique sequence
+    df = organize_data(df)
+
     # Check if predictions already exist
     if os.path.exists(output_path):
         predictions = load_data(output_path)
     else:
-        unique_sequences = test_data[["sequence_id", "sequence"]].drop_duplicates()
-        predictions = []
-        for _, row in tqdm(unique_sequences.iterrows(), total=len(unique_sequences)):
+        # Append to the last column the model predictions
+        df[f"prediction_{args.model_type}"] = pd.Series(dtype='object')
+        for rowidx, row in tqdm(df.iterrows(), total=df.shape[0]):
             pred = predict_structures_arnie(row['sequence'], args.model_type)
-            pred_df = pd.DataFrame({
-                'sequence': [row['sequence']] * len(pred),
-                'sequence_id': [row['sequence_id']] * len(pred),
-                'position_id': range(len(pred)),
-                f'prediction_{args.model_type}': pred
-            })
-            predictions.append(pred_df)
-        predictions = pd.concat(predictions)
-        
+            df.at[rowidx, f"prediction_{args.model_type}"] = array_to_string(pred)
+            if rowidx > 100: break
+
         # Save predictions
-        save_predictions(predictions, output_path)
-    
+        save_predictions(df, output_path)
+
     # Process predictions and compute performance metrics
-    processed_data = process_predictions(predictions, test_data, args.model_type)
     metrics = compute_performance_metrics(processed_data, args.model_type)
     
     # Save and print performance metrics
--- structure/utils.py
+++ structure/utils.py
@@ -2,9 +2,22 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics import f1_score, roc_auc_score, matthews_corrcoef, mean_absolute_error
+from typing import Tuple, List, Dict
+
+def array_to_string(arr, precision=4):
+    x = np.array2string(arr, separator=",", precision=precision)
+    x = x.replace("\n", "").replace(" ", "")
+    return x
+
+def string_to_array(arr_str):
+    x = np.fromstring(arr_str[1:-1], dtype=float, sep=",")
+    return x
 
 def load_data(file_path: str) -> pd.DataFrame:
-    return pd.read_csv(file_path, low_memory=False)
+    if file_path.endswith(".parquet"):
+        return pd.read_parquet(file_path)
+    else:
+        return pd.read_csv(file_path, low_memory=False)
 
 def unpaired_probabilities(prob_matrix: np.ndarray) -> np.ndarray:
     return np.prod(1 - prob_matrix, axis=1)
@@ -16,33 +29,65 @@ def compute_bins(df: pd.DataFrame, model_type: str) -> pd.DataFrame:
     return df
 
 def process_predictions(predictions: pd.DataFrame, test_data: pd.DataFrame, model_type: str) -> pd.DataFrame:
-    merged_data = pd.merge(test_data, predictions, on=['sequence_id', "position_id"], how="left")
+    merged_data = pd.merge(test_data, predictions, on=['seqID', "position_id"], how="left")
     merged_data = merged_data[merged_data[f'prediction_{model_type}'].notna()]
-    merged_data = merged_data[merged_data["Usage"] != "Ignored"]
+    #merged_data = merged_data[merged_data["Usage"] != "Ignored"]
     
     for col in ['reactivity_DMS_MaP', f'prediction_{model_type}']:
         merged_data[col] = merged_data[col].clip(0, 1)
     
     return merged_data.groupby('sequence_id', group_keys=False).apply(compute_bins, model_type=model_type)
 
-def compute_performance_metrics(df: pd.DataFrame, model_type: str) -> dict:
-    true_values = df['reactivity_DMS_MaP_bin']
-    pred_values = df[f'prediction_{model_type}_bin']
-    pred_probs = df[f'prediction_{model_type}']
-    true_reactivities = df['reactivity_DMS_MaP']
+def collate_data(df: pd.DataFrame) -> Tuple[np.ndarray, ...]:
+    """ Compiles the true and predicted reactivity into one long np array """
+    true_reactivities = []
+    pred_probs = []
+    for reactivity, pred in df.values:
+        reactivity = string_to_array(reactivity).clip(0, 1)
+        pred = string_to_array(pred).clip(0, 1)
+        assert(len(pred) == len(reactivity))
+        
+        true_reactivities.append(reactivity)
+        pred_probs.append(pred)
+    true_reactivities = np.hstack(true_reactivities)
+    pred_probs = np.hstack(pred_probs)
     
-    return {
-        'Model Type': model_type,
-        'AUC': roc_auc_score(true_values, pred_probs),
-        'MCC': matthews_corrcoef(true_values, pred_values),
-        'MAE': mean_absolute_error(true_reactivities, pred_probs),
-        'F1-Score': f1_score(true_values, pred_values, average='macro')
-    }
+    # remove nans
+    mask = ~np.isnan(true_reactivities)
+    true_reactivities = true_reactivities[mask]
+    pred_probs = pred_probs[mask]
+    
+    # compute binned values
+    true_values = true_reactivities > np.median(true_reactivities)
+    pred_values = pred_probs > np.median(pred_probs)
+    return true_reactivities, pred_probs, true_values, pred_values
+
+def compute_performance_metrics(predictions: pd.DataFrame, model_type: str) -> List[dict]:
+    metrics = []
+    for chemical_modifier in ["DMS", "2A3"]:
+        reactivity_col = f"reactivity_{chemical_modifier}"
+        df_sub = predictions[[reactivity_col, f"prediction_{model_type}"]]
+        
+        # drop rows with missing reactivity data or missing prediction
+        df_sub = df_sub.dropna()
+
+        true_reactivities, pred_probs, true_values, pred_values = collate_data(df_sub)
+        metric = {
+            'Chemical Modifier': chemical_modifier,
+            'Model Type': model_type,
+            'AUC': roc_auc_score(true_values, pred_probs),
+            'MCC': matthews_corrcoef(true_values, pred_values),
+            'MAE': mean_absolute_error(true_reactivities, pred_probs),
+            'F1-Score': f1_score(true_values, pred_values, average='macro'),
+            'Total sequence length': len(true_reactivities),
+            }
+        metrics.append(metric)
+    return metrics
 
 def save_predictions(predictions: pd.DataFrame, output_path: str):
     predictions.to_csv(output_path, index=False)
 
-def save_performance_metrics(metrics: dict, performance_file: str):
-    metrics_df = pd.DataFrame([metrics])
+def save_performance_metrics(metrics: List[Dict], performance_file: str):
+    metrics_df = pd.DataFrame(metrics)
     print(metrics_df)
     metrics_df.to_csv(performance_file, mode='a', index=False, header=not os.path.exists(performance_file))
