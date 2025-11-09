3993dd852038ff78961c379224127185aba6bcdf
Murphy Angelo <git@murfalo.com>
2025-11-09 13:42:59 -0500

    Removed stale file path for Rfam 15.0 in util/config.py

--- 3d/util/config.py
+++ 3d/util/config.py
@@ -47,7 +47,7 @@ SELF_CONTACT_MIN_NEIGHBOR_DIST = 6  # Min. residue separation for self-contacts
 COVERAGE_RADIUS = 4.5  # Radius used to determine polymer coverage
 
 # Protein/RNA family HMM/CM databases
-RFAM_DIR = "/N/project/alab_rbp/tmp/Rfam-15.0"
+RFAM_DIR = "/Path/to/Rfam-15.0"
 RFAM_CM = f"{RFAM_DIR}/Rfam.cm"
 RFAM_SEED = f"{RFAM_DIR}/Rfam.utf8.seed"
 RFAM_FULL_ALIGNMENT = f"{RFAM_DIR}/alignments/{{accession}}.sto"
